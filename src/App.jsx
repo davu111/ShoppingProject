3b3d9ad2885a2936eca2ce183ad18c2567a4adec
davu111 <vuvanvuonglqm@gmail.com>
2025-03-20 10:23:51 +0700

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Status from "./pages/Status";
 
 function App() {
-  return <></>;
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  return (
+    <Router>
+      <div className="grid grid-cols-12 gap-4 transition-all duration-300">
+        <div
+          className={`transition-all duration-300 ${
+            isCollapsed ? "col-span-1" : "col-span-2"
+          } `}
+        >
+          <Navbar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
+        </div>
+        <div
+          className={`transition-all duration-300 ${
+            isCollapsed ? "col-span-11" : "col-span-10"
+          } `}
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/status" element={<Status />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,102 @@
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../index.css";
+import {
+  faBars,
+  faCartShopping,
+  faHome,
+  faBox,
+  faUser,
+  faSignOut,
+} from "@fortawesome/free-solid-svg-icons";
+import { motion } from "framer-motion";
+
+const paths = ["/", "/cart", "/status", "/profile"];
+
+function Navbar({ isCollapsed, setIsCollapsed }) {
+  const location = useLocation();
+  const [active, setActive] = useState(location.pathname);
+
+  return (
+    <nav className="flex flex-col items-center h-screen bg-gradient-to-b from-indigo-700 to-indigo-900 py-8">
+      <div className="font-mogra underline text-[clamp(1rem,2vw,2.5rem)] text-white flex flex-row items-center">
+        <FontAwesomeIcon
+          icon={faBars}
+          className="mr-2 p-2 rounded-lg bg-indigo-600 cursor-pointer hover:bg-indigo-500"
+          onClick={() => setIsCollapsed(!isCollapsed)}
+        />
+        <span
+          className={`transition-all duration-300 ${
+            isCollapsed ? "hidden" : "block"
+          }`}
+        >
+          ShopNow
+        </span>
+      </div>
+      <div className="relative w-full text-[clamp(1rem,1.5vw,2.5rem)] flex flex-col  font-bold text-white mt-20">
+        <motion.div
+          layoutId="activeTab"
+          className="absolute bottom-0 w-full bg-white"
+          initial={{ top: 0, height: "25%" }} // Mặc định ở Home
+          animate={{
+            top: `${paths.indexOf(active) * 25}%`, // Dịch chuyển ngang theo vị trí tab
+            height: "25%", // Chia đều cho 4 tab
+          }}
+          transition={{ duration: 0.2, ease: "easeInOut" }}
+        />
+        {paths.map((path) => (
+          <Link
+            key={path}
+            to={path}
+            onClick={() => setActive(path)}
+            className={`relative w-full px-8 py-4  cursor-pointer text-center ${
+              active === path ? "text-indigo-600 " : "hover:bg-indigo-600 "
+            }`}
+          >
+            <span className={`relative z-10 flex items-center gap-2 `}>
+              <FontAwesomeIcon
+                icon={
+                  path === "/"
+                    ? faHome
+                    : path === "/cart"
+                    ? faCartShopping
+                    : path === "/status"
+                    ? faBox
+                    : faUser
+                }
+              ></FontAwesomeIcon>
+              <span
+                className={`transition-all duration-300 ${
+                  isCollapsed ? "hidden" : ""
+                }`}
+              >
+                {path === "/"
+                  ? "Home"
+                  : path === "/cart"
+                  ? "Cart"
+                  : path === "/status"
+                  ? "Status"
+                  : "Profile"}
+              </span>
+            </span>
+          </Link>
+        ))}
+      </div>
+      <div className=" w-full text-[clamp(1rem,1.5vw,2.5rem)] font-bold text-white mt-auto">
+        <div className="px-8 py-4 hover:bg-indigo-600 cursor-pointer">
+          <FontAwesomeIcon icon={faSignOut} className="mr-2"></FontAwesomeIcon>
+          <span
+            className={`transition-all duration-300 ${
+              isCollapsed ? "hidden" : ""
+            }`}
+          >
+            Log out
+          </span>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
